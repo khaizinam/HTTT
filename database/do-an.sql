fa48fea1ffdc5deef679306966f36ca39db62483
neriJ <loc_minhhieu@yahoo.com.vn>
2022-01-03 22:02:52 +0700

    Update do-an.sql

--- database/do-an.sql
+++ database/do-an.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2021 at 01:24 PM
+-- Generation Time: Jan 03, 2022 at 03:22 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -43,7 +43,7 @@ CREATE TABLE `item` (
 
 INSERT INTO `item` (`id`, `name`, `sku`, `supplier`, `product_id`, `warehouse_id`, `quantity`) VALUES
 (1, 'Dell XPS 15 9500', 'DXPS159500Q10301121', 'Dell', 2, 1, 2500),
-(2, 'Dell XPS 13 9500', 'DXPS139500QBD281121', 'Dell', 7, 3, 900),
+(2, 'Dell XPS 13 9500', 'DXPS139500QBD281121', 'Dell', 7, 3, 954),
 (3, 'Dell XPS 15 9510', 'DXPS159510Q01281121', 'Dell', 2, 2, 1500),
 (4, 'Dell XPS 13 9510', 'DXPS139510QBD291121', 'Dell', 7, 3, 1500),
 (5, 'Dell XPS 15 9570', 'DXPS159570Q01291121', 'Dell', 2, 2, 2500),
@@ -51,13 +51,15 @@ INSERT INTO `item` (`id`, `name`, `sku`, `supplier`, `product_id`, `warehouse_id
 (7, 'Acer Aspire 5 a515', 'AA5a515QBD301121', 'Acer', 9, 3, 1699),
 (15, 'Acer Aspire 7 a515', 'AA7a515Q01011221', 'Acer', 10, 2, 801),
 (16, 'Dell XPS 15 9570', 'DXPS159570Q01021221', 'Dell', 7, 2, 500),
-(17, 'Acer Aspire 7 a514', 'AA7a514QBD021221', 'Acer', 10, 3, 1649),
-(18, 'Neville Melton', 'Ullamco consequatur', 'In labore officia ip', 9, 5, 445),
+(17, 'Acer Aspire 7 a514', 'AA7a514QBD021221', 'Acer', 10, 3, 1000),
+(18, 'Colleen Walls', 'Magnam accusamus par', 'Cumque quia quis dol', 22, 3, 826),
 (19, 'Michelle Ramsey', 'Exercitation occaeca', 'Ea ab cupiditate et ', 10, 1, 213),
-(20, 'Nora Kerr', 'Dicta dolores autem ', 'Tenetur facere asper', 9, 2, 1248),
+(20, 'Jarrod Ray', 'Quam inventore dolor', 'Quisquam irure molli', 9, 20, 988),
 (21, 'Eliana Merrill', 'Voluptate explicabo', 'Inventore tempor exe', 7, 3, 348),
 (22, 'Vincent Frye', 'Voluptate corporis u', 'Praesentium harum et', 14, 1, 384),
-(23, 'Jocelyn Stanton', 'Sunt possimus eius', 'Enim dolor qui offic', 10, 1, 478);
+(23, 'Jocelyn Stanton', 'Sunt possimus eius', 'Enim dolor qui offic', 10, 1, 478),
+(24, 'Carl Stokes', 'Est eaque voluptates', 'Excepteur ullam hic ', 9, 12, 491),
+(25, 'Castor Stout', 'Id minim expedita pr', 'Nostrum nihil natus ', 7, 16, 606);
 
 --
 -- Triggers `item`
@@ -159,7 +161,10 @@ INSERT INTO `order_items` (`order_id`, `item_id`, `quantity`) VALUES
 (24, 2, 500),
 (24, 3, 500),
 (24, 4, 500),
-(24, 5, 500);
+(24, 5, 500),
+(25, 2, 4),
+(26, 17, 649),
+(27, 2, 50);
 
 -- --------------------------------------------------------
 
@@ -194,7 +199,10 @@ INSERT INTO `order_table` (`id`, `userID`, `order_type`, `order_reason`, `order_
 (21, 1, 1, 'Sed tempora dolor la', 'Voluptatibus officii', 'Natus ipsa odio aut', '2021-12-03 09:35:27'),
 (22, 1, 2, 'Quibusdam expedita u', 'Quidem blanditiis im', 'Voluptatem incididun', '2021-12-03 09:36:34'),
 (23, 1, 2, 'Dolor est do corpor', 'In maiores in fuga ', 'Itaque a est cillum ', '2021-12-03 11:59:43'),
-(24, 1, 1, 'Dolor sint quia ani', 'Neque tempora duis b', 'Quia culpa aut tempo', '2021-12-03 12:00:48');
+(24, 1, 1, 'Dolor sint quia ani', 'Neque tempora duis b', 'Quia culpa aut tempo', '2021-12-03 12:00:48'),
+(25, 1, 1, '', '', '', '2021-12-18 12:03:23'),
+(26, 1, 2, 'Eius quis quia excep', 'Autem omnis qui mole', 'Quis id obcaecati n', '2021-12-18 12:04:49'),
+(27, 1, 1, 'Fugiat veritatis omn', 'Architecto in offici', 'Delectus eu totam v', '2021-12-31 05:14:50');
 
 -- --------------------------------------------------------
 
@@ -216,16 +224,16 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`id`, `name`, `description`, `product_type`, `quantity`) VALUES
 (2, 'Dell XPS 15', 'Laptop hàng đầu thế giới', 'Laptop', 6500),
-(7, 'Dell XPS 13', 'Laptop cho người bình thường', 'Laptop', 3248),
-(9, 'Acer Aspire 5', 'Laptop bình dân', 'Laptop', 4320),
-(10, 'Acer Aspire 7', 'Laptop test edit', 'Laptop', 3141),
+(7, 'Dell XPS 13', 'Laptop cho người bình thường', 'Laptop', 3908),
+(9, 'Acer Aspire 5', 'Laptop bình dân', 'Laptop', 4106),
+(10, 'Acer Aspire 7', 'Laptop test edit', 'Laptop', 2492),
 (14, 'John Manning', 'Expedita quia quasi ', 'Fugiat quia sunt ac', 384),
 (15, 'Stone Herring', 'Voluptates non quaer', 'Quasi repellendus A', 0),
 (16, 'Armando Romero', 'Vel irure ullamco an', 'Ullamco amet ea com', 0),
 (17, 'Freya Fitzpatrick', 'Duis laudantium pos', 'Eius non quo iusto v', 0),
 (18, 'Tanisha Gill', 'Quis quia aliquam ex', 'Maiores omnis ut min', 0),
 (20, 'Iliana Rose', 'Consequatur consect', 'Dolorem magni nisi f', 0),
-(22, 'Adam Williams', 'Ea sapiente eveniet', 'Modi magna ut cum do', 0);
+(22, 'Adam Williams', 'Ea sapiente eveniet', 'Modi magna ut cum do', 826);
 
 -- --------------------------------------------------------
 
@@ -249,7 +257,7 @@ CREATE TABLE `staff` (
 --
 
 INSERT INTO `staff` (`id`, `roleID`, `name`, `address`, `phone_number`, `email`, `username`, `password`) VALUES
-(1, 1, 'Halla Davenport', 'Illum quia recusand', '+1 (645) 616-8326', 'gelulinif@mailinator.com', 'hieu.locminh', 'c4ca4238a0b923820dcc509a6f75849b'),
+(1, 1, 'Leonard Simon', 'Dicta in nihil do ip', '+1 (166) 211-1951', 'cysec@mailinator.com', 'hieu.locminh', 'c4ca4238a0b923820dcc509a6f75849b'),
 (2, 3, 'Adam Holden', 'Voluptatem dolore o', '+1 (458) 146-9439', 'tepy@mailinator.com', 'admin', 'e00cf25ad42683b3df678c61f42c6bda'),
 (6, 2, 'Reed Mckay', 'Aute voluptas distin', '+1 (667) 601-7218', 'xazizazyte@mailinator.com', 'ryjavanixa', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (8, 1, 'Dai Salazar', 'Non consequat Rerum', '+1 (336) 643-1365', 'zijuge@mailinator.com', 'kixox', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
@@ -257,7 +265,6 @@ INSERT INTO `staff` (`id`, `roleID`, `name`, `address`, `phone_number`, `email`,
 (10, 2, 'Chantale Mosley', 'Ut natus nisi ipsum', '+1 (162) 879-1814', 'kakilepota@mailinator.com', 'lyhetetid', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (11, 2, 'Yael Holt', 'Do officia laborum u', '+1 (705) 841-2405', 'wypajyvyg@mailinator.com', 'zofysuxy', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (12, 1, 'Nora Battle', 'Dolore tempora nulla', '+1 (475) 739-6578', 'polymyku@mailinator.com', 'kaqiq', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
-(13, 1, 'Camden Delaney', 'Ipsum consectetur e', '+1 (601) 329-4632', 'badixuda@mailinator.com', 'sobilasuc', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (14, 2, 'Harper Jensen', 'Veniam magna ullam ', '+1 (634) 559-9847', 'vonyhicuh@mailinator.com', 'noloci', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (15, 2, 'Zephr Larson', 'Autem duis numquam o', '+1 (666) 243-5746', 'xejad@mailinator.com', 'mosit', 'f3ed11bbdb94fd9ebdefbaf646ab94d3'),
 (17, 2, 'Joseph Garner', 'Officiis dolorum tem', '+1 (283) 146-2967', 'bucananiq@mailinator.com', 'rumeno', 'f3ed11bbdb94fd9ebdefbaf646ab94d3');
@@ -347,31 +354,31 @@ ALTER TABLE `warehouse`
 -- AUTO_INCREMENT for table `item`
 --
 ALTER TABLE `item`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `order_table`
 --
 ALTER TABLE `order_table`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `warehouse`
 --
 ALTER TABLE `warehouse`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
